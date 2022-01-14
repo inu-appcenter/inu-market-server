201b77afffd8f292411f489a3367fe589a4eae11
juhwan0815 <juwom0831@naver.com>
2022-01-15 00:14:12 +0900

    feat: CORS 설정

--- market-api/src/main/java/inu/market/config/WebConfig.java
+++ market-api/src/main/java/inu/market/config/WebConfig.java
@@ -3,6 +3,7 @@ package inu.market.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +17,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
     }
 }
