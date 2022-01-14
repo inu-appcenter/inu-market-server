3a3f326c111ebb795cf7fa1a3700f50474332655
juhwan0815 <juwom0831@naver.com>
2022-01-14 20:12:49 +0900

    feat: 이미지 업로드 구현

---
+++ market-aws-client/src/main/java/inu/market/client/AwsClient.java
@@ -0,0 +1,44 @@
+package inu.market.client;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class AwsClient {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    public String bucket;
+
+    public String upload(MultipartFile file) {
+        String fileName = createFileName(file.getContentType());
+
+        try {
+            amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), null)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+            return amazonS3Client.getUrl(bucket, fileName).toString();
+        } catch (IOException e) {
+            throw new RuntimeException("파일 업로드 중 오류가 발생했습니다.");
+        }
+    }
+
+    private String createFileName(String contentType) {
+        return UUID.randomUUID().toString() + "." + extractExt(contentType);
+    }
+
+    private String extractExt(String contentType) {
+        int pos = contentType.lastIndexOf("/");
+        return contentType.substring(pos + 1);
+    }
+
+}
