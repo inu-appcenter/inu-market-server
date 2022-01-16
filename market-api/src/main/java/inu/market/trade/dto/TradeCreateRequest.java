096a0dfafd6d3c3d207684d9bacd3e6024aaaaee
juhwan0815 <juwom0831@naver.com>
2022-01-17 00:06:41 +0900

    refactor: 거래 검증 설정

--- market-api/src/main/java/inu/market/trade/dto/TradeCreateRequest.java
+++ market-api/src/main/java/inu/market/trade/dto/TradeCreateRequest.java
@@ -4,12 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class TradeCreateRequest {
 
+    @NotNull(message = "회원 ID는 필수입니다.")
+    @Positive(message = "회원 ID는 양수이어야 합니다.")
     private Long userId;
 
+    @NotNull(message = "상품 ID는 필수입니다.")
+    @Positive(message = "상품 ID는 양수이어야 합니다.")
     private Long itemId;
 }
