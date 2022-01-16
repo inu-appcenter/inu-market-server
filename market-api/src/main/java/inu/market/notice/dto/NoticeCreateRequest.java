b178d8c34bbc5777261141764951234d8f28ba3c
juhwan0815 <juwom0831@naver.com>
2022-01-17 00:06:19 +0900

    refactor: 공지사항 검증 설정

--- market-api/src/main/java/inu/market/notice/dto/NoticeCreateRequest.java
+++ market-api/src/main/java/inu/market/notice/dto/NoticeCreateRequest.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class NoticeCreateRequest {
 
+    @NotBlank(message = "제목은 필수입니다.")
     private String title;
 
+    @NotBlank(message = "제목은 필수입니다.")
     private String content;
 }
