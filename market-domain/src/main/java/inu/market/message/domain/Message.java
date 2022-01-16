a4ec7c3407b5c50c903bf845eec63bf7ff8662cc
juhwan0815 <juwom0831@naver.com>
2022-01-16 22:33:53 +0900

    refactor: 채팅 메세지 JPA Auditing 설정

--- market-domain/src/main/java/inu/market/message/domain/Message.java
+++ market-domain/src/main/java/inu/market/message/domain/Message.java
@@ -6,11 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
