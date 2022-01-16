6885b2772587448163437328f63ca52e9ae1798c
juhwan0815 <juwom0831@naver.com>
2022-01-17 00:13:07 +0900

    refactor: 알림 응답 DTO 설정

--- market-api/src/main/java/inu/market/notification/dto/NotificationResponse.java
+++ market-api/src/main/java/inu/market/notification/dto/NotificationResponse.java
@@ -1,9 +1,12 @@
 package inu.market.notification.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import inu.market.notification.domain.Notification;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -20,11 +23,15 @@ public class NotificationResponse {
 
     private Long referenceId;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createdAt;
+
     public static NotificationResponse from(Notification notification) {
         return new NotificationResponse(notification.getId(),
                                         notification.getContent(),
                                         notification.isRead(),
                                         notification.getNotificationType().getType(),
-                                        notification.getReferenceId());
+                                        notification.getReferenceId(),
+                                        notification.getCreatedAt());
     }
 }
