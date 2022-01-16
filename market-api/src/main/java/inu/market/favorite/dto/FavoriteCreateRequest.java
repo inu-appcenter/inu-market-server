8e8c4eb40959831c32eaea4c1789b5cda0af83cb
juhwan0815 <juwom0831@naver.com>
2022-01-16 23:25:58 +0900

    refactor: 찜 검증 설정

--- market-api/src/main/java/inu/market/favorite/dto/FavoriteCreateRequest.java
+++ market-api/src/main/java/inu/market/favorite/dto/FavoriteCreateRequest.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class FavoriteCreateRequest {
 
+    @NotNull(message = "상품 ID는 필수입니다.")
+    @Positive(message = "상품 ID는 양수이어야 합니다.")
     private Long itemId;
 }
