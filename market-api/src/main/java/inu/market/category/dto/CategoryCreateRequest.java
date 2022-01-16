e27b93647f3631497b2f2615ea885ec4667285bf
juhwan0815 <juwom0831@naver.com>
2022-01-16 23:25:46 +0900

    refactor: 카테고리 검증 설정

--- market-api/src/main/java/inu/market/category/dto/CategoryCreateRequest.java
+++ market-api/src/main/java/inu/market/category/dto/CategoryCreateRequest.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CategoryCreateRequest {
 
+    @NotBlank(message = "이름은 필수입니다.")
     private String name;
 
+    @NotBlank(message = "아이콘 이미지는 필수입니다.")
     private String iconUrl;
+
 }
