e4e573177875468348eaea01fa2bd91bc866a8b1
juhwan0815 <juwom0831@naver.com>
2022-01-21 14:00:34 +0900

    refactor: AccessDeniedException import 수정

--- market-api/src/main/java/inu/market/advice/GlobalExceptionHandler.java
+++ market-api/src/main/java/inu/market/advice/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package inu.market.advice;
 import inu.market.common.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.nio.file.AccessDeniedException;
 import java.util.HashMap;
 import java.util.Map;
 
