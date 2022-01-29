261a2b111f9f5b6ef97e91826384206fb18f3264
juhwan0815 <juwom0831@naver.com>
2022-01-29 19:46:42 +0900

    fix: JPA Auditing 설정 추가

--- market-domain/src/main/java/inu/market/config/JpaConfig.java
+++ market-domain/src/main/java/inu/market/config/JpaConfig.java
@@ -1,7 +1,9 @@
 package inu.market.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing
 public class JpaConfig {
 }
